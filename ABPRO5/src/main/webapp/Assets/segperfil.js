39b21127f3398b3a78e9decbfc47705ab0115d4d
Cristian Carrillo <naitsircl@gmail.com>
2023-04-05 12:08:16 -0400

    formulario crearUsuario con cliente, Pro y ADm

--- ABPRO5/src/main/webapp/Assets/segperfil.js
+++ ABPRO5/src/main/webapp/Assets/segperfil.js
@@ -1,28 +1,16 @@
-// Get a reference to the select element and the form container
-const tipoSelect = document.getElementById('tipo');
-const formContainer = document.getElementById('form-container');
-
-// Listen for changes to the select element
-tipoSelect.addEventListener('change', () => {
-  // Get the value of the selected option
-  const tipo = tipoSelect.value;
-
-  // Send an AJAX request to get the appropriate form
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `loadform.jsp?tipo=${tipo}`);
-  xhr.onload = () => {
-    // Hide all other forms
-    const forms = document.querySelectorAll('.formulario');
-    forms.forEach(form => {
-      if (form.classList.contains(`formulario-${tipo}`)) {
-        form.classList.add('formulario-activo');
-      } else {
-        form.classList.remove('formulario-activo');
-      }
-    });
-
-    // Replace the content of the form container with the loaded form
-    formContainer.innerHTML = xhr.response;
-  };
-  xhr.send();
-});
+function displayFormFields() {
+    var selectedOption = document.getElementById("tipo").value;
+    if (selectedOption == "cli") {
+        document.getElementById("formulario-cli").classList.remove("d-none");
+        document.getElementById("formulario-pro").classList.add("d-none");
+        document.getElementById("formulario-adm").classList.add("d-none");
+    } else if (selectedOption == "pro") {
+        document.getElementById("formulario-cli").classList.add("d-none");
+        document.getElementById("formulario-pro").classList.remove("d-none");
+        document.getElementById("formulario-adm").classList.add("d-none");
+    } else if (selectedOption == "adm") {
+        document.getElementById("formulario-cli").classList.add("d-none");
+        document.getElementById("formulario-pro").classList.add("d-none");
+        document.getElementById("formulario-adm").classList.remove("d-none");
+    }
+}
